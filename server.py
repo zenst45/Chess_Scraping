73578bdae5a27b113f9184afac3b7e6c4e32e13c
noe <noenabon41@gmail.com>
2025-08-07 01:54:50 +0200

    flask host ip

--- server.py
+++ server.py
@@ -14,4 +14,4 @@ def get():
     return [others.count_players()[0], others.count_games()]
 
 if __name__ == '__main__':
-    app.run(debug=True, port=12346)
+    app.run(host="0.0.0.0", debug=True, port=12346)
