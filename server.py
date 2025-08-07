4286a6057b953b1c8b2fb4c2184fff52cacdc962
noe <noenabon41@gmail.com>
2025-08-07 02:23:09 +0200

    server missing get function

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from threading import Thread
 from flask import Flask
 import get_players
 import get_games
+import others
 
 app = Flask(__name__)
 
@@ -16,5 +17,9 @@ def update_players():
 
     return "Mise à jour lancée en arrière-plan", 202  # Code 202 = Accepted
 
+@app.route('/api/get/', methods=['GET'])
+def get():
+    return [others.count_players()[0], others.count_games()]
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=12346, debug=False)
+    app.run(host="0.0.0.0", port=12346, debug=False,)
