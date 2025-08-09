ee501ae05f66c16120ef8fc6dd9255417a4c95aa
noe <noenabon41@gmail.com>
2025-08-09 02:25:22 +0200

    update get_players.py with GMs

--- get_players.py
+++ get_players.py
@@ -1,17 +1,13 @@
 import requests
 import json
+import get_games
 
 def main():
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0'}
     with open('top_players.json', 'r', encoding='utf-8') as f:
         players = json.load(f)
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0'
-    }
-    leaderboards = requests.get("https://api.chess.com/pub/leaderboards", headers=headers).json()
-    # with open('data.json', 'r', encoding='utf-8') as f:
-    #     leaderboards = json.load(f)
-    # print(leaderboards)
 
+    leaderboards = requests.get("https://api.chess.com/pub/leaderboards", headers=headers).json()
     # Scroll des leaderboards
     for leaderboard in leaderboards.values():
         # Scroll des joueurs
@@ -20,7 +16,17 @@ def main():
                 players["players"].append(player)
                 players["ids"].append(player["player_id"])
 
-    top_countries = requests.get("https://api.chess.com/pub/leaderboards", headers=headers).json()
+    gms = requests.get("https://api.chess.com/pub/titled/GM", headers=headers).json()
+    print(len(gms["players"]))
+    i = 0
+    for player in gms["players"]:
+        i += 1
+        print(i)
+        player_id = requests.get(f"https://api.chess.com/pub/player/{player}", headers=headers).json()["player_id"]
+        if player_id not in players["ids"]:
+            players["players"].append(player)
+            players["ids"].append(player_id)
 
     with open('top_players.json', 'w', encoding='utf-8') as f:
         json.dump(players, f, ensure_ascii=False, indent=2)
+    get_games.main()
